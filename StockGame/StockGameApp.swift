2204570f98196566fe0421c890bef3e90b74c025
Kolby Croshaw <kolbycroshaw@gmail.com>
2022-07-28 10:50:07 -0600

    Initial Commit

---
+++ StockGame/StockGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockGameApp.swift
+//  StockGame
+//
+//  Created by Kolby Croshae on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct StockGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
